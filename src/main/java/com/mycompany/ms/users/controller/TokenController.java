c34d4566ca7d02963e0ae40c324cab12eaf2cc37
kirill <liriescsgo@gmail.com>
2025-01-03 13:09:54 +0300

    project .isReady

---
+++ src/main/java/com/mycompany/ms/users/controller/TokenController.java
@@ -0,0 +1,43 @@
+package com.mycompany.ms.users.controller;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TokenController {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @GetMapping("/validate-token")
+    public ResponseEntity<String> validateToken(@RequestParam("token") String token) {
+        boolean isValid = isTokenValid(token);
+
+        if (isValid) {
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("{\"message\": \"Token is valid\"}");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).contentType(MediaType.APPLICATION_JSON).body("{\"message\": \"Invalid token\"}");
+        }
+    }
+
+
+    public boolean isTokenValid(String token) {
+        try {
+            JWTVerifier verifier = JWT.require(com.auth0.jwt.algorithms.Algorithm.HMAC256(secret))
+                    .build();
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT != null;
+        } catch (JWTVerificationException exception) {
+            return false;
+        }
+    }
+}
--- src/main/java/com/mycompany/ms/users/service/TokenService.java
+++ src/main/java/com/mycompany/ms/users/service/TokenService.java
@@ -10,17 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class TokenService {
 
-    @Value("${jwt.secret}")
-    private String secret;
 
-    public boolean validateToken(String token) {
-        try {
-            JWTVerifier verifier = JWT.require(com.auth0.jwt.algorithms.Algorithm.HMAC256(secret))
-                    .build();
-            DecodedJWT decodedJWT = verifier.verify(token); 
-            return decodedJWT != null;
-        } catch (JWTVerificationException exception) {
-            return false;
-        }
-    }
+
+
 }
